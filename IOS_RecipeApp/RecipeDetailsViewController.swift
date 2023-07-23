6a32f9511fabd38076755f8003d9aaef35bfacc8
yuenshan <A00270323@mycambrian.ca>
2023-07-23 00:28:21 -0400

    Improving the UI of the details page

--- IOS_RecipeApp/RecipeDetailsViewController.swift
+++ IOS_RecipeApp/RecipeDetailsViewController.swift
@@ -13,29 +13,43 @@ class RecipeDetailsViewController: UIViewController {
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var areaLabel: UILabel!
     @IBOutlet weak var ingredientLabel: UILabel!
-    @IBOutlet weak var measureLabel: UILabel!
     @IBOutlet weak var instructionsLabel: UILabel!
     
     var recipe: Recipe?
-         
-         override func viewDidLoad() {
-             super.viewDidLoad()
-             
-             if let selectedRecipe = recipe {
-                         updateUI(with: selectedRecipe)
-                     } else {
-                         print("No recipe data received.")
-                     }
-         }
-         
-         private func updateUI(with recipe: Recipe) {
-             mealLabel.text = recipe.strMeal
-             categoryLabel.text = recipe.strCategory
-             areaLabel.text = recipe.strArea
-             ingredientLabel.text = recipe.ingredientsAndMeasures.joined(separator: ", ")
-             instructionsLabel.text = recipe.strInstructions
-         }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let selectedRecipe = recipe {
+            updateUI(with: selectedRecipe)
+        } else {
+            print("No recipe data received.")
+        }
+    }
+    
+    private func updateUI(with recipe: Recipe) {
+        mealLabel.text = recipe.strMeal
+        categoryLabel.text = recipe.strCategory
+        areaLabel.text = recipe.strArea
 
+        // Combine ingredients and measures into a single string
+        var ingredientsString = ""
+        for i in 0..<recipe.ingredientsAndMeasures.count {
+            let ingredient = recipe.ingredientsAndMeasures[i]
+            if !ingredient.isEmpty {
+                if !ingredientsString.isEmpty {
+                    ingredientsString += ", "
+                }
+                ingredientsString += ingredient
+            }
+        }
+        ingredientLabel.text = ingredientsString
+        instructionsLabel.text = recipe.strInstructions
+    }
+
+
+    
+    
     /*
      // MARK: - Navigation
      
